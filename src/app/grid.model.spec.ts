8891198fffca326990d0d4c3b6cdc66f5aa88cf0
kumarshanu19 <er.kumarshanu@gmail.com>
2020-03-07 17:58:35 +0530

    final

---
+++ src/app/grid.model.spec.ts
@@ -0,0 +1,7 @@
+import { DynamicGrid } from './grid.model';
+
+describe('DynamicGrid', () => {
+  it('should create an instance', () => {
+    expect(new DynamicGrid()).toBeTruthy();
+  });
+});
---
+++ src/app/grid.model.ts
@@ -0,0 +1,12 @@
+export class DynamicGrid{     
+    _id: string;
+    service_date:Date;  
+    product:string;  
+    hsn:number;  
+    sku: string;
+    description: string;
+    qty:number;
+    rate: number;
+    amount: number;
+    tax: number;
+}
